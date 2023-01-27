9a88da7302cfa18bb54f04f2ac60db6fb212dbc2
kinoshitalab <kouiti@arakkk.local>
2023-01-27 17:28:27 +0900

    update

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 sudo apt install -y make
 sudo apt install -y entr
 sudo apt install -y fish
+sudo apt install -y git
 
 # Install python
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 mkdir ~/.config
-ln -fsn ~/.config/config.fish ./config.fish
+cp ./config.fish ~/.config/config.fish 
 source ~/.config/config.fish
 pyenv install 3.10
 # Install poetry
