9ab7ea082165e721106db5be85e14c91996b64a5
kinoshitalab <kouiti@arakkk.local>
2023-01-27 17:22:05 +0900

    update

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 sudo apt install -y make
 sudo apt install -y entr
 sudo apt install -y fish
-curl -sS https://starship.rs/install.sh | sh
-fish
-starship init fish | source
 
 # Install python
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
@@ -14,3 +11,6 @@ source ~/.config/config.fish
 pyenv install 3.10
 # Install poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+# install starship
+curl -sS https://starship.rs/install.sh | sh
