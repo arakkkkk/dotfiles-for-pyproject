a649f3d6bbbcbc4273989137edc57e46fea0f077
kinoshitalab <kouiti@arakkk.local>
2023-02-02 16:51:16 +0900

    update

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ touch ~/.config/fish/config.fish
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-eval ". ~/.bashrc"
+eval "source ~/.bashrc"
 
 pyenv install 3.10
 # Install poetry
