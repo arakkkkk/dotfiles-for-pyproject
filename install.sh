60fb4f3493275dbea8fd07ab468c866f727a603f
kinoshitalab <kouiti@arakkk.local>
2023-02-02 09:39:25 +0900

    update

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ sudo apt install -y \
   libsqlite3-dev \
   libopencv-dev
   # tk-dev
-fish
 
 # Install python
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
@@ -24,18 +23,12 @@ touch ~/.config/fish/config.fish
 echo "set -x PYENV_ROOT $HOME/.pyenv" >> ~/.config/fish/config.fish
 echo "set -x PATH  $PYENV_ROOT/bin $PATH" >> ~/.config/fish/config.fish
 echo "pyenv init - | source" >> ~/.config/fish/config.fish
-source ~/.config/config.fish
-pyenv install 3.10
-pyenv global 3.10
 # Install poetry
 curl -sSL https://install.python-poetry.org | python -
 
 # Install fish config
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install jethrokuan/z
-fisher install jethrokuan/fzf
 
 # install starship
 echo "starship init fish | source" >> ~/.config/fish/config.fish
 curl -sS https://starship.rs/install.sh | sh
-source ~/.config/config.fish
