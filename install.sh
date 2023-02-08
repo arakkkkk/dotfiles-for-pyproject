eb32e006477f0bb694b3df7d45b8dd01c8c7e9c8
kinoshitalab <kouiti@arakkk.local>
2023-02-08 09:30:31 +0900

    update

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ sudo apt install -y \
   fish \
   git \
   xdotool \
-  python3-poetry \
-  python3-cachecontrol \
-  mysql-server \
-  mysql-client \
+  libmysqlclient-dev
+
+# Install python
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+sudo apt install -y \
   build-essential \
   libffi-dev \
   libssl-dev \
@@ -21,10 +22,7 @@ sudo apt install -y \
   libsqlite3-dev \
   libopencv-dev \
   tk-dev
-
-# Install python
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' > ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 eval "source ~/.bash_profile"
@@ -32,7 +30,9 @@ eval "source ~/.bash_profile"
 pyenv install 3.10
 pyenv global 3.10
 # Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
+sudo apt install -y \
+  python3-poetry \
+  python3-cachecontrol
 
 # Install fish config
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
@@ -45,8 +45,8 @@ eval "source ~/.bash_profile"
 # for fish
 mkdir -p ~/.config/fish
 touch ~/.config/fish/config.fish
-echo "set -x PYENV_ROOT $HOME/.pyenv" >> ~/.config/fish/config.fish
-echo "set -x PATH  $PYENV_ROOT/bin $PATH" >> ~/.config/fish/config.fish
+echo "set -x PYENV_ROOT \$HOME/.pyenv" > ~/.config/fish/config.fish
+echo "set -x PATH  \$PYENV_ROOT/bin \$PATH" >> ~/.config/fish/config.fish
 echo "pyenv init - | source" >> ~/.config/fish/config.fish
 echo "starship init fish | source" >> ~/.config/fish/config.fish
 
